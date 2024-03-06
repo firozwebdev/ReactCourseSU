d09414b8371d6d14e959360fd1f431a5bdaf1df2
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-03-07 01:50:12 +0600

    practiced

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -1,12 +1,17 @@
-import Gallery from "./components/Gallery";
+import Gallery, { Profile } from "./components/Gallery";
 //1.5.P1
+/*
+ import Gallery, { Profile } from "./components/Gallery"
+ Here Gallery is default import because gallery is defined as default export
+ import Gallery, { Profile } from "./components/Gallery";
+ But Here Profile is named import because Profile is defined as named export
+ */
+//
 function App() {
   return (
     <div>
       <h1>Main File here..</h1>
-      <Gallery />
-      <Gallery />
-      <Gallery />
+      <Profile
     </div>
   );
 }
--- reactCourse/rnext/src/components/Gallery.jsx
+++ reactCourse/rnext/src/components/Gallery.jsx
@@ -1,4 +1,4 @@
-function Profile() {
+export function Profile() {
     return (
         <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
     );
