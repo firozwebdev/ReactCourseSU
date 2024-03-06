bbe36b02e97595608de8a07602067b774a02e330
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-03-07 01:59:07 +0600

    practice

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -5,6 +5,12 @@ import Gallery, { Profile } from "./components/Gallery";
  Here Gallery is default import because gallery is defined as default export
  import Gallery, { Profile } from "./components/Gallery";
  But Here Profile is named import because Profile is defined as named export
+ 
+ import Gallery from "./components/Gallery"; 
+ As Gallery is default import so we can call it by any name 
+ import { Profile }  from "./components/Gallery"; 
+ As Profile is default import so we can't call it by any name but we use aliase
+ import { Profile as MyProfile }  from "./components/Gallery"; 
  */
 //
 function App() {
@@ -12,6 +18,7 @@ function App() {
     <div>
       <h1>Main File here..</h1>
       <Profile />
+      <Gallery />
     </div>
   );
 }
