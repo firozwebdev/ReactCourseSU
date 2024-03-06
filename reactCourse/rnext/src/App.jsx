7694ced5568b30a580f602de6f5946f0dc2bb4f3
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-03-07 00:49:35 +0600

    file added

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -1,14 +1,12 @@
 import Gallery from "./components/Gallery";
 //main
 function App() {
-    return (
-        <div>
-            <h1>Main File here..</h1>
-            <Gallery />
-            <Gallery /> 
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>Main File here..</h1>
+      <Gallery />
+    </div>
+  );
 }
 
 export default App;
