3d4fe756c107434835b0a12043a39252c668fcde
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-03-07 00:59:41 +0600

    file added

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -1,15 +1,14 @@
 import Gallery from "./components/Gallery";
+import Buttons from "./components/Test";
 //1.4.P1
 function App() {
-    return (
-        <div>
-            <Gallery />
-            <Gallery />
-            <Gallery />
-            <Gallery />
-           
-        </div>
-    );
+  return (
+    <div>
+      <h1>This is for practice</h1>
+      <Gallery />
+      <Buttons />
+    </div>
+  );
 }
 
 export default App;
