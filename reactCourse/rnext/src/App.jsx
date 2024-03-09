679f1015910559f0b84365191ff4eb52ee565bd9
Sabuz <everythingkst@gmail.com>
2024-03-09 15:00:20 +0600

    file added

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -1,8 +1,11 @@
+import Profile from "./components/Profile";
+
 //1.8.P1
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Profile />
     </div>
   );
 }
---
+++ reactCourse/rnext/src/components/demo.jsx
@@ -0,0 +1,8 @@
+/*#__PURE__*/
+React.createElement(
+  "div",
+  null /*#__PURE__*/,
+
+  React.createElement("h1", null, "Hello"),
+  React.createElement("p", null, "World")
+);
