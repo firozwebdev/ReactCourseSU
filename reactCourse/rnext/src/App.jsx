873afb39b74ff36784240f5e2d709a96a7c30aa0
Sabuz <everythingkst@gmail.com>
2024-03-26 20:57:23 +0600

    working...

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -1,46 +1,50 @@
-
-
-import { useState } from 'react';
-import AddTask from './components/AddTask';
-import TaskList from './components/TaskList';
-import {initialTasks}  from './data/task.js';
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import { initialTasks } from "./data/task.js";
 
 export default function App() {
-  const [tasks,setTasks] = useState(initialTasks)
+  const [tasks, setTasks] = useState(initialTasks);
 
   const getNextId = (data) => {
-    const maxId = data.reduce((prev,current) => prev && prev.id > current.id ? prev.id : current.id)
-    return maxId + 1 ;
-  }
+    const maxId = data.reduce((prev, current) =>
+      prev && prev.id > current.id ? prev.id : current.id
+    );
+    return maxId + 1;
+  };
   //handlers
-   const handleAddTask = (text) => {
-     setTasks([
+  const handleAddTask = (text) => {
+    setTasks([
        ...tasks,
        {
-        id: getNextId(tasks), 
-        text: text, 
+        id: getNextId(tasks),
+        text: text,
         done: false,
        }
      ])
-   };
-   const handleChangeTask = (task) => {
-    const nextTasks = tasks.map(t =>{
+  };
+  const handleChangeTask = (task) => {
+    const nextTasks = tasks.map((t) => {
+      if(t.id === task.id){
+        return task //new task
+      }else{
+        return t; //old task
+      }
+    });
+    setTasks(nextTasks);
+  };
 
-    })
-     setTasks([])
-   }
+  const handleDeleteTask = (taskId) =>{
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
 
   return (
     <>
       <h1>Prague itinerary</h1>
-      <AddTask onClick={handleAddTask}/>
-      <TaskList tasks={tasks}/>
+      <AddTask onAdd={handleAddTask} />
+      <TaskList tasks={tasks} />
     </>
   );
-
 }
 
 let nextId = 3;
-
-
-
