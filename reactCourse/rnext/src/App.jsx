fba3738293d85c87aa54a336dd8b478b0e3b2b8b
Sabuz <everythingkst@gmail.com>
2024-03-26 16:07:46 +0600

    project set up initially...

--- reactCourse/rnext/src/App.jsx
+++ reactCourse/rnext/src/App.jsx
@@ -4,53 +4,28 @@
 import { useState } from 'react';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
-import {initialTasks}  from './data.js';
+import {initialTasks}  from './data/task.js';
 
-function App() {
+export default function App() {
   const [tasks, setTasks] = useState(initialTasks);
 
   function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
+
   }
 
-  function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    );
-  }
-
-  function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
-  }
+  function handleChangeTask(task) {}
 
   return (
     <>
       <h1>Prague itinerary</h1>
       <AddTask />
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
-      />
+      <TaskList />
     </>
   );
+
 }
 
 let nextId = 3;
 
 
 
-export default App;
--- reactCourse/rnext/src/components/AddTask.jsx
+++ reactCourse/rnext/src/components/AddTask.jsx
@@ -6,16 +6,9 @@ export default function AddTask({onAddTask}) {
     <>
       <input
         placeholder="Add task"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        
       />
-      <button
-        onClick={() => {
-          setText('');
-          onAddTask(text);
-        }}>
-        Add
-      </button>
+      <button> Add </button>
     </>
   );
 }
